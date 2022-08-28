5779e778211a9836089185d993ad4cd7d0ec665c
ArBe <argon-beryllium@protonmail.com>
2022-08-28 04:35:55 +0200

    added some light optimisation (performace is still, expectedly, abysmal)

--- main.lua
+++ main.lua
@@ -82,6 +82,38 @@ function EvalLeapPoint(perms, leap_point, len)
 	end
 	return out
 end
+function EvalAllLeapPoints(perms, len)
+	local out = {}
+	for i=1,len do out[i] = 0 end
+	for _,p in pairs(perms)
+	do
+		local broken = {}
+		local best_yet = {}
+		for i=1,len do best_yet[i] = 0 end
+
+		for i=1,len
+		do
+			if best_yet[i] < p[i] then
+				for j=i,len do
+					best_yet[j] = p[i]
+				end
+			end
+
+			for j=1,i
+			do
+				if(broken[j]) then goto cont end
+				if p[i] >= best_yet[j] then
+					if p[i] == len then
+						out[j] = out[j] + 1
+					end
+					broken[j] = true
+				end
+				::cont::
+			end
+		end
+	end
+	return out
+end
 
 local l = 9
 
@@ -90,10 +122,9 @@ print('Generating '..perfect..' permutations...')
 local perms = GenPermutations(l)
 print('Done.')
 print('Evaluating possible leap points...')
-local results = {}
+local results = EvalAllLeapPoints(perms, l)
 local winner = 1
 for i=1,l do
-	results[i] = EvalLeapPoint(perms, i, l)
 	if results[i] > results[winner] then
 		winner = i
 	end
